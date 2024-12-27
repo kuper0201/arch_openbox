2df50878731fdf752927c38d792156fa6278e79a
kuper0201 <kuper171@gmail.com>
2024-12-27 15:48:16 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# https://yjwang.tistory.com/165
+
+# install something
+pacman -Syu --needed nano git base-devel i3-gaps xorg-server xorg-xinit pcmanfm-gtk3 greetd-tuigreet lxappearance alacritty xdotool
+
+# install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -rf yay
+
+# greeter setting
+# nano /etc/greetd/config.toml => command = "tuigreet --cmd /bin/bash"
+
+systemctl enable greetd
+
+yay -Syu polybar rofi dunst compton
