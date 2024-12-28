05357620db4973b777d5b58cc3603ebf5d3b215d
kuper0201 <kuper171@gmail.com>
2024-12-28 16:38:37 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # https://yjwang.tistory.com/165
 
 # install something
-pacman -Syu --needed nano git base-devel i3-gaps xorg-server xorg-xinit pcmanfm-gtk3 greetd-tuigreet lxappearance alacritty xdotool
+pacman -Syu --needed --noconfirm nano git base-devel i3-gaps xorg-server xorg-xinit pcmanfm-gtk3 greetd-tuigreet lxappearance alacritty xdotool
 
 # install yay
 git clone https://aur.archlinux.org/yay.git
@@ -16,4 +16,4 @@ rm -rf yay
 
 systemctl enable greetd
 
-yay -Syu polybar rofi dunst compton ttf-hack-nerd --needed --noconfirm
+yay -Syu polybar rofi dunst compton feh ttf-hack-nerd --needed --noconfirm
