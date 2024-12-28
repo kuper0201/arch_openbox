c650837d63312ef8ff918d6c1a7593c771074544
kuper0201 <kuper171@gmail.com>
2024-12-28 17:11:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-# https://yjwang.tistory.com/165
+# check root and exit
+if [ "$EUID" -eq 0 ]; then
+    echo "Do not run this script as root!"
+    echo "Exiting..."
+    exit 1
+fi
 
 # install something
-pacman -Syu --needed --noconfirm nano git base-devel jq i3-gaps xorg-server xorg-xinit pcmanfm-gtk3 greetd-tuigreet lxappearance alacritty xdotool
+sudo pacman -Syu --needed --noconfirm nano git base-devel jq i3-gaps xorg-server xorg-xinit pcmanfm-gtk3 greetd-tuigreet lxappearance alacritty xdotool
 
 # install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+sudo makepkg -si
 rm -rf yay
 
 # greeter setting
-# nano /etc/greetd/config.toml => command = "tuigreet --cmd /bin/bash"
-sed -i 's|command = "agreety --cmd /bin/sh"|command = "tuigreet --cmd /bin/bash"|' /etc/greetd/config.toml
+sudo sed -i 's|command = "agreety --cmd /bin/sh"|command = "tuigreet --cmd /bin/bash"|' /etc/greetd/config.toml
+sudo systemctl enable greetd
 
-systemctl enable greetd
-
+# install packages from AUR
 yay -Syu polybar rofi dunst compton feh ttf-hack-nerd --needed --noconfirm
 
 # install keybinds and themes
@@ -28,4 +32,5 @@ mv scripts ~/scripts
 mv dot_config ~/.config
 mv dot_themes ~/.themes
 
+# set gtk3 theme
 # lxappearance
