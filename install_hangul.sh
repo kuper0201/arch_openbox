93c9d2b66a6b83a89db3f0697c1bc86f6bcecfaf
kuper0201 <kuper171@gmail.com>
2025-01-26 01:22:07 +0900

    Update install_hangul.sh

--- install_hangul.sh
+++ install_hangul.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu --needed --noconfirmfcitx5 fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-qt fcitx5-gtk
+sudo pacman -Syu --needed --noconfirm fcitx5 fcitx5-im fcitx5-hangul fcitx5-configtool fcitx5-qt fcitx5-gtk
 
 sudo sed -i "$ s/$/\nGTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx/" /etc/environment
 
