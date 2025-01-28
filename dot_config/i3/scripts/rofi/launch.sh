51917eaa01e9083ee1aeab5ed7c1e0b4a4425883
kuper0201 <kuper171@gmail.com>
2025-01-28 15:21:28 +0900

    update rofi launch script

--- dot_config/i3/scripts/rofi/launch.sh
+++ dot_config/i3/scripts/rofi/launch.sh
@@ -5,9 +5,9 @@ menu="$1"
 if [ "$menu" = "appmenu" ]; then
     rofi -show drun -theme clean
 elif [ "$menu" = "powermenu" ]; then
-    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -30 -yoffset 100
+    rofi -modi 'Powermenu:~/.config/i3/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -30 -yoffset 100
 elif [ "$menu" = "tabmenu" ]; then
-    ~/scripts/rofi/tabmenu.sh
+    ~/.config/i3/scripts/rofi/tabmenu.sh
 elif [ "$menu" = "scratchpad" ]; then
-    rofi -show scratchpad -modi 'scratchpad:~/scripts/rofi/scratchpad.sh' -theme clean
+    rofi -show scratchpad -modi 'scratchpad:~/.config/i3/scripts/rofi/scratchpad.sh' -theme clean
 fi
