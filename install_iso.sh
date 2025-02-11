0b9c589ac81cbc7362044d5e8865964a36e6a107
kuper0201 <kuper171@gmail.com>
2025-02-11 16:27:25 +0900

    Update install_iso.sh

--- install_iso.sh
+++ install_iso.sh
@@ -22,6 +22,7 @@ find "$TARGET_DIR" -type f -exec chown root:root {} \;
 sed -i "$ s/$/\nQT_QPA_PLATFORMTHEME=qt5ct\nQT_STYLE_OVERRIDE=kvantum\nGTK_THEME=Materia-dark/" /etc/environment
 sed -i "$ s/$/\nGTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx/" /etc/environment
 sed -i "$ s/$/\nPATH=\/opt\/miniconda3\/bin/" /etc/environment
+sed -i "$ s/$/\nCRYPTOGRAPHY_OPENSSL_NO_LEGACY=1/" /etc/environment
 
 # lightdm 배경화면 설정
 sed -i 's|^#background.*|background=/usr/share/wallpapers/wall_1.png|' /etc/lightdm/lightdm-gtk-greeter.conf
